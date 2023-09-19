cb07fda630e23333320d77a59f3a5219c6500be8
suhunlin <lin.suhun@msa.hinet.net>
2023-09-19 09:34:37 +0800

    Initial TextView and add tag to show life cycle

--- app/src/main/java/com/suhun/startstyleservice/MainActivity.java
+++ app/src/main/java/com/suhun/startstyleservice/MainActivity.java
@@ -3,21 +3,68 @@ package com.suhun.startstyleservice;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
+    private String tag = MainActivity.class.getSimpleName();
+    private TextView showCounter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        Log.d(tag, "-----#####MainActivity onCreate#####-----");
         setContentView(R.layout.activity_main);
     }
 
+    private void initView(){
+        showCounter = findViewById(R.id.lid_showCounter);
+    }
+
+    @Override
+    protected void onStart() {
+        super.onStart();
+        Log.d(tag, "-----#####MainActivity onStart#####-----");
+    }
+
+    @Override
+    protected void onRestart() {
+        super.onRestart();
+        Log.d(tag, "-----#####MainActivity onRestart#####-----");
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        Log.d(tag, "-----#####MainActivity onResume#####-----");
+    }
+
+    @Override
+    protected void onPause() {
+        super.onPause();
+        Log.d(tag, "-----#####MainActivity onPause#####-----");
+    }
+
+    @Override
+    protected void onStop() {
+        super.onStop();
+        Log.d(tag, "-----#####MainActivity onStop#####-----");
+    }
+
+    @Override
+    protected void onDestroy() {
+        super.onDestroy();
+        Log.d(tag, "-----#####MainActivity onDestroy#####-----");
+    }
+
     public void startServiceFun(View view){
+        Log.d(tag, "-----#####MainActivity startServiceFun#####-----");
 
     }
 
     public void stopServiceFun(View view){
+        Log.d(tag, "-----#####MainActivity stopServiceFun#####-----");
 
     }
 }
--- app/src/main/java/com/suhun/startstyleservice/MyService.java
+++ app/src/main/java/com/suhun/startstyleservice/MyService.java
@@ -3,14 +3,33 @@ package com.suhun.startstyleservice;
 import android.app.Service;
 import android.content.Intent;
 import android.os.IBinder;
+import android.util.Log;
 
 public class MyService extends Service {
-    public MyService() {
-    }
+    String tag = MyService.class.getSimpleName();
 
     @Override
     public IBinder onBind(Intent intent) {
         // TODO: Return the communication channel to the service.
-        throw new UnsupportedOperationException("Not yet implemented");
+        Log.d(tag, "-----*****MyService onBind*****-----");
+        return null;
+    }
+
+    @Override
+    public void onCreate() {
+        Log.d(tag, "-----*****MyService onCreate*****-----");
+        super.onCreate();
+    }
+
+    @Override
+    public int onStartCommand(Intent intent, int flags, int startId) {
+        Log.d(tag, "-----*****MyService onStartCommand*****-----");
+        return super.onStartCommand(intent, flags, startId);
+    }
+
+    @Override
+    public void onDestroy() {
+        super.onDestroy();
+        Log.d(tag, "-----*****MyService onDestroy*****-----");
     }
 }
